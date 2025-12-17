3df54b961230cd468179aa3ed00b96294dcf1aff
MaksymPidhornyi <maksym.ldc@gmail.com>
2025-12-17 19:53:07 +0200

    add js

--- src/js/api.js
+++ src/js/api.js
@@ -1,50 +1,43 @@
-import axios from "axios";
+import axios from 'axios';
 
-const URL = "https://paw-hut.b.goit.study";
+export const URL = 'https://paw-hut.b.goit.study';
 
-const animalsURL = `${URL}/api/animals`;
-const categoriesURL = `${URL}/api/categories`;
+export const animalsURL = `${URL}/api/animals`;
+export const categoriesURL = `${URL}/api/categories`;
+
 const ordersURL = `${URL}/api/orders`;
 const feedbacksURL = `${URL}/api/feedbacks`;
 
 export async function getAnimals({ page = 1, limit = 10, categoryId }) {
-    const params = { page, limit };
-    if (categoryId) {
+  const params = { page, limit };
+  if (categoryId) {
     params.categoryId = categoryId;
     // Если категория выбрана, добавь ее в запрос, если нет, то не добавляй ее вообще.
-    // Потому что, нет категории "ВСЕ" 
+    // Потому что, нет категории "ВСЕ"
+  }
+  const response = await axios.get(animalsURL, { params });
+  return response.data;
+  // При вызове передаете в аргументах первой порции карточек page, limit, а для фильтра categoryId
 }
-    const response = await
-        axios.get(animalsURL, { params });
-    return response.data;
-    // При вызове передаете в аргументах первой порции карточек page, limit, а для фильтра categoryId
-};
 
 export async function getCategories() {
-    const response = await
-        axios.get(categoriesURL);
-    return response.data;
-
-    
-};
+  const response = await axios.get(categoriesURL);
+  return response.data;
+}
+//
 
 export async function createOrder(orderData) {
-    const response = await
-        axios.post(ordersURL, orderData);
-    return response.data;
-    
-};
+  const response = await axios.post(ordersURL, orderData);
+  return response.data;
+}
 
 export async function getFeedbacks(limit = 10, page = 1) {
-    const response = await
-        axios.get(feedbacksURL, {
-            params: {
-                page,
-                limit,
-            }
-        })
-    return response.data;
-    // При вызове функции передаете в аргументы лимит и страничку
-    
-};
-
+  const response = await axios.get(feedbacksURL, {
+    params: {
+      page,
+      limit,
+    },
+  });
+  return response.data;
+  // При вызове функции передаете в аргументы лимит и страничку
+}
